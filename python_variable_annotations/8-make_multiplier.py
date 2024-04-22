5d37b2f3237e718bf290d19bd57cbc7548f79935
AudreyLican <audrey.lican@gmail.com>
2024-04-22 16:44:32 +0200

    code style fix

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -4,7 +4,7 @@ and returns a function that multiplies a float by multiplier"""
 from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable[[float   ], float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     def multiplier_func(a: float) -> float:
         return a * multiplier
     return multiplier_func
