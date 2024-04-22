50db3433b32e2b4e8e333b3cc9913590ff19ee16
AudreyLican <audrey.lican@gmail.com>
2024-04-22 16:35:04 +0200

    missing argument in return and update on comments

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""Function concat, for concatenating 2 string 'str1' and 'str2'."""
+"""Function floor, that takes float as argument
+and returns the floor of the float."""
 import math
 
 
 def floor(n: float) -> float:
-    return math.floor
+    return math.floor(n)
