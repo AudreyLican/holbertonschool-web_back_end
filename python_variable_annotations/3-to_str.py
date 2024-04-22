8e6648ad70a5d99c9fb71c0cf54baa75d360b722
AudreyLican <audrey.lican@gmail.com>
2024-04-22 16:52:08 +0200

    update comments style

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-"""Function to_str, that take a float as argument,
-and return the string representation of the float."""
+"""
+Function to_str, that take a float as argument,
+and return the string representation of the float.
+"""
 
 
 def to_str(n: float) -> str:
