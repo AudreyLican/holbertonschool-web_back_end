2b44e893cfad94694ba1e28c6e38b55cd1b96cf6
AudreyLican <audrey.lican@gmail.com>
2024-04-22 16:48:43 +0200

    remplace str by sequence

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Function make_multiplier, take a float 'multiplier' 
 and returns a function that multiplies a float by multiplier"""
-from typing import Tuple, Iterable, List
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: Iterable[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     return [(i, len(i)) for i in lst]
