56baafb95748bffa829c1d49b007246d73c52750
AudreyLican <audrey.lican@gmail.com>
2024-04-22 17:02:47 +0200

    change float by int

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -4,6 +4,6 @@ and returns the floor of the float."""
 import math
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """return the floor of the float n"""
     return math.floor(n)
