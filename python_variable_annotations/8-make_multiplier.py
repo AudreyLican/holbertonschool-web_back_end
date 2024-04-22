8f0e85b764a597e3c2dbff108525a514b1c5d4d6
AudreyLican <audrey.lican@gmail.com>
2024-04-22 17:13:27 +0200

    added comments in second method

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """
-Function make_multiplier, takes a float 'multiplier' and 
+make_multiplier, takes a float 'multiplier' and 
 returns a function that multiplies a float by multiplier"""
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """returns a function that multiplies a floar by multiplier"""
+    """returns a function that multiplies a float by multiplier"""
     def multiplier_func(a: float) -> float:
+        """float multiply by multiplier"""
         return a * multiplier
     return multiplier_func
