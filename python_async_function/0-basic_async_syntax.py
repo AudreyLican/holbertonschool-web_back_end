a098a4b63c90ea66a32033b68c039b4c5fbea373
AudreyLican <audrey.lican@gmail.com>
2024-04-23 16:55:41 +0200

    update code style

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-"""Create an asynchronous coroutine that take
-a in as argument"""
+"""
+    Create an asynchronous coroutine that take
+    a in as argument"""
 import asyncio
 import random
 
 
-async def wait_random(max_delay: int=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """Coroutine waitinf for random delay btw 0 and max_delay"""
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
+    random_float = random.uniform(0, max_delay)
+    await asyncio.sleep(random_float)
+    return random_float
