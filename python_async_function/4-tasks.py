24e312966ec2031e61a02efed4adff58e087b1ac
AudreyLican <audrey.lican@gmail.com>
2024-04-23 17:08:54 +0200

    Hot fix

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -12,7 +12,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """ Tasks"""
+    """ Tasks wait_n"""
     delays: List[float] = []
     all_delays: List[float] = []
 
