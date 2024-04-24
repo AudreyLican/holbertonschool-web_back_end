50a37fa5eb2655327971015ccf0077bf325a5051
AudreyLican <audrey.lican@gmail.com>
2024-04-24 02:54:56 +0200

    update import

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,9 +8,10 @@
 import asyncio
 from typing import List
 
-async_generator = __import__('0-async_generator.py').async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
     """Async comprehension"""
-    return [i async for i in async_generator()]
+    x = [i async for i in async_generator()]
+    return x
